c5a0eb324a2732f3875f09a908bcc12cb1449a84
Prathmesh Jadhav <115774608+Pratham3753@users.noreply.github.com>
2023-09-12 01:26:28 +0530

    script.js
    first

--- script.js
+++ script.js
@@ -25,6 +25,34 @@ function loader(){
   document.querySelector('.loader-container').classList.add('fade-out');
 }
 
+
+function SendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    number: document.getElementById("number").value,
+    foodName: document.getElementById("foodName").value,
+    address: document.getElementById("address").value,
+  };
+
+  const serviceID = "service_sp8y90l";
+  const templateID = "template_msjswlt";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("number").value = "";
+        document.getElementById("foodName").value = "";
+        document.getElementById("address").value = "";
+        console.log(res);
+        alert("Thank You For your order! Your support is very appriciated")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
 function fadeOut(){
   setInterval(loader, 3000);
 }
